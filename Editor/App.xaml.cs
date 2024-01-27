72eb6e54b3550746feb0cea2ce434f62efc802f7
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-01-27 14:44:13 +0500

    Add project files.

---
+++ Editor/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Editor.View.Window;
+using System.Windows;
+
+namespace Editor;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow mainWindow = new();
+
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+}
---
+++ Editor/View/Window/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Editor.View.Window;
+
+public partial class MainWindow : System.Windows.Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
