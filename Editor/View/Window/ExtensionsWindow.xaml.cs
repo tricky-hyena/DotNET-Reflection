d15dd16a958e01adb893dd6dd861b164ce9cd55f
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-01 12:00:33 +0300

    Fix build.

--- Editor/View/Window/ExtensionsWindow.xaml.cs
+++ Editor/View/Window/ExtensionsWindow.xaml.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿namespace Editor.View.Window;
 
-namespace Editor.View.Window
+public partial class ExtensionsWindow : System.Windows.Window
 {
-    /// <summary>
-    /// Interaction logic for ExtensionsWindow.xaml
-    /// </summary>
-    public partial class ExtensionsWindow : Window
+    public ExtensionsWindow()
     {
-        public ExtensionsWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
