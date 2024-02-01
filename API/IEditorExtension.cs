47be63d10e4196b9fd7487fefe4386e582dc7d95
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-01 12:30:53 +0300

    Add API library project (IEditorExtension, VersionAttribute).

---
+++ API/IEditorExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace API
+{
+    internal interface IEditorExtension
+    {
+        string Title { get; }
+
+        string AuthorName { get; }
+
+        string Description { get; }
+
+        void Transform(Bitmap bitmap);
+    }
+}
---
+++ API/VersionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime;
+
+namespace API
+{
+    internal class VersionAttribute : Attribute
+    {
+        public int Major { get; }
+
+        public int Minor { get; }
+
+        public int Patch { get; }
+
+        public VersionAttribute(int major, int minor, int patch)
+        {
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+        }
+    }
+}
