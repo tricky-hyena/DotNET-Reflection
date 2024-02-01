1d67c544eec364a3a34044e27adceba43cba245d
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-01 12:03:58 +0300

    Add OpenExtensionsWindowCommand.

--- Editor/ViewModel/Window/MainWindowViewModel.cs
+++ Editor/ViewModel/Window/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Editor.Command;
+using Editor.View.Window;
 using System.Windows;
 using System.Windows.Input;
 
@@ -14,9 +15,22 @@ namespace Editor.ViewModel.Window
         private void exit(object parameter)
             => Application.Current.Shutdown();
 
+        public ICommand OpenExtensionsWindowCommand { get; set; }
+
+        private bool canOpenExtensionsWindow(object parameter)
+            => true;
+
+        private void openExtensionsWindow(object parameter)
+        {
+            ExtensionsWindow extensionsWindow = new();
+
+            extensionsWindow.ShowDialog();
+        }
+
         public MainWindowViewModel()
         {
             ExitCommand = new RelayCommand(canExit, exit);
+            OpenExtensionsWindowCommand = new RelayCommand(canOpenExtensionsWindow, openExtensionsWindow);
         }
     }
 }
