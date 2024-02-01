f15b7cd5b06a52ffad6319584c050a95e573bd18
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-01 19:57:12 +0300

    Add opening image from file.

--- ImageEditor/View/Window/MainWindow.xaml.cs
+++ ImageEditor/View/Window/MainWindow.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace ImageEditor.View.Window
+﻿using Microsoft.Win32;
+using System.Drawing;
+
+namespace ImageEditor.View.Window
 {
     public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenImage(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+                PictureBox.Image = new Bitmap(dialog.FileName);
         }
     }
 }
