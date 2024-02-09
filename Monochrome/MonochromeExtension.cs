da5f2a0bd0a5ba964f10c1fb4a0d692dede0ded2
tricky-hyena <157110410+tricky-hyena@users.noreply.github.com>
2024-02-09 22:56:42 +0300

    Add second extension.

---
+++ Monochrome/MonochromeExtension.cs
@@ -0,0 +1,55 @@
+﻿using API;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monochrome
+{
+
+    [ExtensionVersion(1, 0, 0)]
+    public class MonochromeExtension : IImageEditorExtension
+    {
+        public string Title => "Monochrome Extension";
+
+        public string AuthorName => "No Name";
+
+        public string Description => Title;
+
+        public Bitmap Transform(Bitmap image)
+        {
+            float b = 2.0f;
+            ColorMatrix cm = new ColorMatrix(new float[][]
+                {
+                    new float[] {0.3f, 0.3f, 0.3f, 0, 0},
+                    new float[] { 0.59f, 0.59f, 0.59f, 0, 0},
+                    new float[] { 0.11f, 0.11f, 0.11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1},
+                });
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(cm);
+
+            // Draw the image onto the new bitmap while applying the new ColorMatrix.
+            Point[] points =
+            {
+                new Point(0, 0),
+                new Point(image.Width, 0),
+                new Point(0, image.Height),
+            };
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            // Make the result bitmap.
+            Bitmap bm = new Bitmap(image.Width, image.Height);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.DrawImage(image, points, rect, GraphicsUnit.Pixel, attributes);
+            }
+
+            return bm;
+        }
+    }
+}
